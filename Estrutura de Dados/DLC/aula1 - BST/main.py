097db449422d3af237a6244a9e020b6c0522a981
Serra <79658532+Fabioserrasv@users.noreply.github.com>
2023-08-16 00:46:52 +0000

    + Adicionando exclussao de Node na BST

--- Estrutura de Dados/DLC/aula1 - BST/main.py
+++ Estrutura de Dados/DLC/aula1 - BST/main.py
@@ -60,12 +60,67 @@ class Bst:
                         continue
                     aux.left = Node(value)
                     break
+    
+    def __min_node(self, node):
+        if node is None:
+            return None
+
+        current = node
+        while current.left is not None:
+            current = current.left
+
+        return current
+
+    def deleteNode(self, node, value):
+        if node is None:
+            return None
+
+        if value < node.value:
+            node.left = self.deleteNode(node.left, value)
+            return node
+
+        if value > node.value:
+            node.right = self.deleteNode(node.right, value)
+            return node
+
+        if node.left is None and node.right is None:
+            return None
+
+        if node.left is None or node.right is None:
+            return node.right if node.left is None else node.left
+
+        aux = self.__min_node(node.right)
+        aux_value = aux.value
+
+        result_remove_node = self.deleteNode(node.right, aux_value)
+
+        new_node = Node(aux_value)
+        new_node.left = node.left
+        new_node.right = result_remove_node
+        return new_node
 
 bst = Bst()
 
-valores = (np.random.randint(0,1000,(1,8)))[0]
+valores = (np.random.randint(0,99,(1,8)))[0]
 
-for i in range(0, 8):
-    bst.add_value(valores[i])
+# for i in range(0, 6):
+#     bst.add_value(valores[i])
+
+
+#75,42,82,11,74,32,69,56
+bst.add_value(75)
+bst.add_value(42)
+bst.add_value(82)
+bst.add_value(11)
+bst.add_value(74)
+bst.add_value(32)
+bst.add_value(69)
+bst.add_value(56)
+
 
 tree_printer(bst)
+print("-"*8)
+bst.deleteNode(bst.root, 42)
+tree_printer(bst)
+
+print("deleted:" + str(42))
